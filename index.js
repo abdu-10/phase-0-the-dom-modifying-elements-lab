5be78af9f586082557fa05efffc73b29618ffdb3
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-12-02 17:51:00 +0300

    compeleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+
+
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.setAttribute('id', 'victory')
+
+
+newHeader.textContent= "Abdu is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
